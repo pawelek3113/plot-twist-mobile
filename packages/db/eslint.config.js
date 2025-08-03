d17fbe9347d452b00900c5f6e05d521295aa3a47
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2025-08-04 00:25:46 +0200

    chore: reset repo

--- packages/db/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@plot-twist/eslint-config/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist/**"],
-  },
-  ...baseConfig,
-];
